b83df2d5eacec8d10a5def8bff3b64e2639ab901
Mihail Larionov <ms.larionovv@gmail.com>
2025-05-10 18:33:55 +0300

    maven

---
+++ untitled/src/test/java/TestMain.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+
+
+    @Test
+    public void test() {
+        Assertions.assertEquals(10, 10);
+    }
+}
